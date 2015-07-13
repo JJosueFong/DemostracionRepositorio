c475e13ad88d834a16f343e81684d26a80789880
Work <Work@JavierFong-PC>
2015-07-12 23:42:25 -0600

    Segunda Modificacion HT1

--- HojaTrabajo1/src/hojatrabajo1/HojaTrabajo1.java
+++ HojaTrabajo1/src/hojatrabajo1/HojaTrabajo1.java
@@ -11,11 +11,16 @@ package hojatrabajo1;
  */
 public class HojaTrabajo1 {
 
+    String nombre; 
+    int valor; 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    }
+    public String getNombre() {
+        return nombre; 
     }
     
 }
